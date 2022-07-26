7f0750181704893e0ea939fcbecf590beae3500f
RexMortem <68436000+RexMortem@users.noreply.github.com>
2022-07-26 17:26:17 +0100

    Update RSA.lua
    Re-edited some things; turns out that usually RSA does the power before the modulus so added a function for that (think it was a computerphile thing for demonstrating the clock). Laid out the framework, now investigating programming RSA itself

--- RSA.lua
+++ RSA.lua
@@ -1,12 +1,53 @@
 --[[
 	Source code inspired by ComputerPhile, and researched sources 
+	
+	Euler's totient function gives number of coprime integers with N
+	but when you have 2 prime factor-pairs then you can just do (p - 1)(q - 1)
 --]]
 
 local RSAModule = {
 	
 }
 
+function RSAModule:EulerTotient(n) -- the brute force way (not (p - 1)(q - 1)) lmao
+	local factors = {}
+	
+	for i = 2,math.sqrt(n) do
+		if (n % i) == 0 then
+			table.insert(factors, i)
+			table.insert(factors, n/i)
+		end
+	end
+	
+	local LenFactors = #factors
+	local count = 0
+	
+	for i = 1, n-1 do
+		local coprime = true
+		
+		for d = 1,LenFactors do
+			if (i % factors[d]) == 0 then
+				coprime = false
+				break
+			end
+		end
+		
+		if coprime then
+			count += 1
+		end
+	end
+	
+	return count
+end
+
 function RSAModule:ModulusClock(x, p, modulo)
+	--[[
+		The p acts as a public key
+		The reverse value for p is the private key
+		
+		From a computerphile video
+	--]]
+
 	local oX = x -- since x will change
 	
 	for i = 2, p do
@@ -14,6 +55,27 @@ function RSAModule:ModulusClock(x, p, modulo)
 	end
 	
 	return x
+end
+
+function RSAModule:EncryptNumber(x, n, e)
+	return math.pow(x, n) % e
+end
+
+function RSAModule:Encrypt(x, n, e)
+	
+end
+
+function RSAModule:BruteForceKey(p, modulo)
+	
+end
+
+function RSAModule:GeneratePublicKey(p, q)
+	local n = p * q
+end
+
+function RSAModule:GeneratePrivateKey(p, q)
+	local EulerTotient = (p - 1) * (q - 1)
+	
 end
 
 return RSAModule
